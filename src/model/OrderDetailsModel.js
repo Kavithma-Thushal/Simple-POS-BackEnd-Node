019a9cdb42a7a255054e6cc01d491657be8e6fa9
Kavithma-Thushal <kavithmathushal9007@gmail.com>
2024-08-31 21:19:54 +0530

    Updated

--- src/model/OrderDetailsModel.js
+++ src/model/OrderDetailsModel.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const orderDetailsSchema = new mongoose.Schema({
-    orderId: { type: String, ref: 'Order', required: true },
-    itemCode: { type: String, ref: 'Item', required: true },
-    buyQty: { type: Number, required: true },
-    total: { type: Number, required: true }
+    orderId: {type: String, ref: 'Order', required: true},
+    itemCode: {type: String, ref: 'Item', required: true},
+    buyQty: {type: Number, required: true},
+    total: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('OrderDetails', orderDetailsSchema);
